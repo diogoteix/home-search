929f531b110fd7d4ccbe44e8e834794ae6b89177
Diogo Teixeira <teixeira.diogo5@gmail.com>
2020-05-25 14:47:53 +0100

    init

---
+++ build/precache-manifest.394d567e8483fadc35db2778c4a21f59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "882dce6bf919fe8487d91b7a9d725fbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87298d2488d3c5edbb42",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "090b9213d73e119c8568",
+    "url": "/static/js/2.2c258f22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2c258f22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87298d2488d3c5edbb42",
+    "url": "/static/js/main.3de3c341.chunk.js"
+  },
+  {
+    "revision": "b434bfa7846dce6fe856",
+    "url": "/static/js/runtime-main.deb0a028.js"
+  }
+]);
